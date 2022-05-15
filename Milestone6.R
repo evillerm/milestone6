88f9d50fb5c9a418c2bef625f8307667990bcb3b
evillerm <105471952+evillerm@users.noreply.github.com>
2022-05-15 17:52:11 -0400

    Adding changes

--- Milestone6.R
+++ Milestone6.R
@@ -9,7 +9,7 @@ download.file(dropBoxURL,destFile, mode = "wb")
 ## read all data
 liveBankAcct = read_csv('liveBankAcct.csv')
 liveCustomerList = read_csv('liveCustomerList.csv')
-sampleBankAccInput = read_csv(destFile)
+sampleBankAccInput = read_csv(destFile) # This is be read in by URL
 # This is not being used for the main data manipulation
 sampleBankAcctSolution = read_csv('sampleBankAcctSolution.csv')
 
